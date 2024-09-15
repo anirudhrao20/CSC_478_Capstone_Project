bee6b58c65806cc4ae15a99cb5c826720649a113
Anirudh Rao <contactme@anirudhrao.dev>
2024-09-15 14:28:34 -0700

    Created temp home screen, added a company data object as per https://finnhub.io/docs/api/company-profile2

---
+++ portfolio_tracker_flutter/lib/models/company.dart
@@ -0,0 +1,16 @@
+class Company {
+  String country;
+  String currency;
+  String exchange;
+  String ipo;
+  int marketCapitalization;
+  String name;
+  String phone;
+  double shareOutstanding;
+  String ticker;
+  String weburl;
+  String logo;
+  String finnhubIndustry;
+
+  Company(this.country, this.currency, this.exchange, this.ipo, this.marketCapitalization, this.name, this.phone, this.shareOutstanding, this.ticker, this.weburl, this.logo, this.finnhubIndustry);
+}
